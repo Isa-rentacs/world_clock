def3f284d02bbac0820a0e7765f28018258f0245
Junnosuke Kuroda <kurodaju@SEA-1800781736.ant.amazon.com>
2021-10-01 00:40:57 -0700

    Initial version

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Clock tz="PST"/>
+  <Clock tz="UTC"/>
+  <Clock tz="JST"/>
+  <Clock tz="EST"/>
+  <Clock tz="CET"/>
+  <Clock tz="IST"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Clock from './components/Clock.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Clock
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Clock.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>{{tz}}:{{time_str}}</div>
+</template>
+
+<script>
+export default {
+    name: 'Clock',
+    props: {
+        tz: String
+    },
+    data: function(){
+        return {
+            time_str: new Date().toLocaleString("en-US", {timeZone: this.tz})
+        }
+    },
+    mounted: function() {
+        this.timer = setInterval(() => {
+            this.time_string()
+        }, 1000)
+    },
+    methods: {
+        time_string: function() {
+            this.time_str = new Date().toLocaleString("en-US", {timeZone: this.tz})
+        }
+    }
+}
+</script>
